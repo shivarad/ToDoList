a65d1bdcdd0f190b66fc843aabccefd3f0c10959
shivarad <shiva.rrad@gmail.com>
2021-01-20 00:13:43 +0330

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Fragment ,lazy,Suspense } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Loading from './components/loading/loading';
-import TodoPage from './TodoPage/TodoPage';
-// const TodoPage=lazy(()=>import('./TodoPage/TodoPage'));
+
+const TodoPage=lazy(()=>import('./TodoPage/TodoPage'));
  const App=()=> {
    return(
     <BrowserRouter>
     <Fragment>
-        {/* <Suspense fallback={<Loading/>}> */}
+        <Suspense fallback={<div>loading...</div>}>
           <Route exact path="/" component={TodoPage}/>
-        {/* </Suspense> */}
+        </Suspense>
       </Fragment>
   </BrowserRouter>
       
